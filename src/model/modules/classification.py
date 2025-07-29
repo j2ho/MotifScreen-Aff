40a94bac2131e3735a68a9a3d8144778c3ab4286
j2ho <92702929+j2ho@users.noreply.github.com>
2025-07-29 17:24:45 +0900

    remove unused model options. now use former_contrast

--- src/model/modules/classification.py
+++ src/model/modules/classification.py
@@ -1,7 +1,5 @@
 import torch
 import torch.nn as nn
-from src.model.utils import masked_softmax
-
 
 class ClassModule( nn.Module ):
     def __init__(self, m, c,
@@ -10,32 +8,8 @@ class ClassModule( nn.Module ):
         super().__init__()
         # m: originally called "embedding_channels"
         self.classification_mode = classification_mode
-        
-        if self.classification_mode == 'ligand':
-            self.lapool = nn.AdaptiveMaxPool2d((4,c)) #nn.Linear(l0_out_features,c)
-            self.rapool = nn.AdaptiveMaxPool2d((100,c))
-            self.lhpool = nn.AdaptiveMaxPool2d((20,c))
-            self.linear_pre_aff = nn.Linear(c,1)
-            self.linear_for_aff = nn.Linear(124,1)
-            
-        elif self.classification_mode in ['ligand_v2','ligand_v3','combo_v1']:
-            self.wra = nn.Linear(m, m) #, bias=None)
-            self.wrh = nn.Linear(m, m) #, bias=None)
-            self.wla = nn.Linear(m, m)#, bias=None)
-            self.wlh = nn.Linear(m, m)#, bias=None)
-            self.linear_z1 = nn.Linear(m,1)
-            M = {'ligand_v2':2*m,'ligand_v3':2*m+n_lig_emb,'combo_v1':2*m+n_lig_emb}[self.classification_mode]
-            L = {'ligand_v2':3,'ligand_v3':5,'combo_v1':5}[self.classification_mode]
 
-            if self.classification_mode == 'combo_v1':
-                self.w_cR = nn.Linear( m, m )
-                self.w_cl = nn.Linear( m, m )
-                self.linear_kR = nn.Linear( m, m )
-            
-            self.map_to_L = nn.Linear(M,L)
-            self.final_linear = nn.Linear(L, 1)
-
-        elif self.classification_mode == 'former':
+        if self.classification_mode == 'former':
             self.linear_z1 = nn.Linear(c, 1)
             self.map_to_L = nn.Linear( 2*c+n_lig_emb , 8 )
             self.final_linear = nn.Linear( 8, 1 )
@@ -49,27 +23,13 @@ class ClassModule( nn.Module ):
             
             self.Gamma = nn.Parameter( torch.Tensor([0.1]) )
             
-        elif self.classification_mode == 'tank':
-            # attention matrix to affinity 
-            self.linear1 = nn.Linear(m, 1)
-            self.linear2 = nn.Linear(m, 1)
-            self.linear1a = nn.Linear(m, m)
-            self.linear2a = nn.Linear(m, m)
-            self.bias = nn.Parameter(torch.ones(1))
-            self.leaky = nn.LeakyReLU()
             
     def forward( self, z, hs_grid_batched, hs_key_batched,
                  lig_rep=None,
-                 hs_rec_batched=None,
-                 w_Rl=None, w_mask=None ):
-
-        ## TODO
-        # for classification
+                 w_mask=None ):
 
         if self.classification_mode == 'former':
-            ## simplified!
             att = self.linear_z1(z).squeeze(-1)
-            #z = torch.einsum('bnh,bkh->bnk', hs_grid_batched, hs_key_batched) # b x N x k
             
             att_l = torch.nn.Softmax(dim=2)(att).sum(axis=1) # b x K
             att_r = torch.nn.Softmax(dim=1)(att).sum(axis=2) # b x N
@@ -78,12 +38,11 @@ class ClassModule( nn.Module ):
             key_rep = torch.einsum('bk,bkl -> bl', att_l, hs_key_batched)
             grid_rep = torch.einsum('bi,bil -> bl',att_r, hs_grid_batched)
 
-            #
             pair_rep = torch.cat([key_rep, grid_rep, lig_rep ],dim=1) # b x emb*2
             pair_rep = self.map_to_L(pair_rep) # b x L
             Aff = self.final_linear(pair_rep).squeeze(-1) # b x 1
             
-        elif self.classification_mode in ['former_contrast','former_contrast2']:
+        elif self.classification_mode == 'former_contrast':
             eps = 1.0e-9
             # normalized embedding across channel dim
             hs_grid_batched = torch.softmax(hs_grid_batched, axis=-1) # B x N x d
@@ -96,14 +55,10 @@ class ClassModule( nn.Module ):
 
             ## 2) per-key aff part
             # "attention-weighted 1-D probability"
-            # opt1
             key_P = torch.einsum('bnkd,bkd -> bnk', z, hs_key_batched) #the original unnormed "z" should make more sense
             
             #max per each key across grid; range 0~10
             key_P = nn.functional.max_pool2d(key_P,kernel_size=(key_P.shape[-2],1)) # B x 1 x k 
-            #if self.classification_mode == 'former_contrast3':
-            #    Aff = torch.sigmoid(torch.mean(key_P,axis=(1,2)))
-            #else:
             aff_key = self.Pcoeff*(key_P + self.Poff).squeeze(dim=1) # B x k
 
             # Pcoeff: 0.3, Poff: -1.8, aff_key: -0.5~0.5
@@ -113,29 +68,5 @@ class ClassModule( nn.Module ):
             Aff = ( aff_key + self.Gamma*aff_lig )/(1+self.Gamma)
                 
             Aff = ( Aff, Aff_contrast )
-            
-        # tested but forbidden?
-        elif self.classification_mode in ['former_contrast3']:
-            #hs_key_batched: layernormed
-            
-            eps = 1.0e-9
-            # 1) contrast part
-            # derive dot product to binders to be at 1.0, non-binders at 0.0
-
-            # measure just direction
-            hs_key_normed = hs_key_batched / hs_key_batched.norm(dim=-1).unsqueeze(-1)
-            Aff_contrast = torch.einsum( 'bkd,d->bk', hs_key_normed, self.Affmap ) # B x k
-
-            ## 2) per-key aff part
-            # simplest -- measure "per-key-score"
-            # why hs_key_batch becomes < 0?
-            aff_key = torch.einsum( 'bkd,d->bk', hs_key_batched, self.linear_key ).unsqueeze(1) # B x 1 x k
-            #print(hs_key_batched, aff_key)
-            
-            # sum over keys; individual elem can be neg or pos
-            Aff = torch.mean(aff_key*w_mask,axis=(1,2)) #
-            #Aff = torch.sigmoid(Aff) #Aff should be logit, not probability
-
-            Aff = ( Aff, Aff_contrast ) 
             
         return Aff
